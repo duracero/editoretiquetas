da2517b0114de3eb4b86cf4ecc5a24c5fba7070c
nilinking <nilinking@gmail.com>
2026-02-06 15:14:09 +0100

    Github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://duracero.github.io',
+  base: '/editoretiquetas',
   integrations: [preact()],
 
   vite: {
